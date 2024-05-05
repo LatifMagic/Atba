c9511937793dd59bd7fdfd3ac7be57e6ba74fa5d
Abdelatif Bensahnoun <latifben26@gmail.com>
2024-05-05 23:34:47 +0100

    Skeleton loading

--- components/shared/Listing.tsx
+++ components/shared/Listing.tsx
@@ -9,42 +9,49 @@ const Listing = ({ listing }: Props) => {
   return (
     <div>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-        {listing?.map((item, index) => (
-          <div
-            key={index}
-            className="border-2 border-gray-200 shadow-sm rounded-xl cursor-pointer 
+        {listing?.length > 0
+          ? listing?.map((item, index) => (
+              <div
+                key={index}
+                className="border-2 border-gray-200 shadow-sm rounded-xl cursor-pointer 
             transition ease-in-out duration-300 hover:border-primary"
-          >
-            <Image
-              src={item?.listingImages[0]?.url}
-              alt=""
-              width={800}
-              height={800}
-              className="object-cover rounded-t-lg h-[170px]"
-            />
-            <div className="flex flex-col gap-2 mt-2 p-2">
-              <h2 className="text-lg font-semibold">{item?.price} DZD</h2>
-              <h2 className="flex gap-1 text-sm text-gray-400">
-                <MapPin className="h-4 w-4" />
-                {item?.address}
-              </h2>
-              <div className="flex gap-2 mt-2">
-                <h2 className="flex gap-2 text-sm justify-center items-center text-gray-600">
-                  <BedDouble className="h-4 w-4" />
-                  {item?.bedroom}
-                </h2>
-                <h2 className="flex gap-2 text-sm justify-center items-center text-gray-600 border-x-2 px-2">
-                  <Bath className="h-4 w-4" />
-                  {item?.bathroom}
-                </h2>
-                <h2 className="flex gap-2 text-sm justify-center items-center text-gray-600">
-                  <Ruler className="h-4 w-4" />
-                  {item?.area}
-                </h2>
+              >
+                <Image
+                  src={item?.listingImages[0]?.url}
+                  alt=""
+                  width={800}
+                  height={800}
+                  className="object-cover rounded-t-lg h-[170px]"
+                />
+                <div className="flex flex-col gap-2 mt-2 p-2">
+                  <h2 className="text-lg font-semibold">{item?.price} DZD</h2>
+                  <h2 className="flex gap-1 text-sm text-gray-400">
+                    <MapPin className="h-4 w-4" />
+                    {item?.address}
+                  </h2>
+                  <div className="flex gap-2 mt-2">
+                    <h2 className="flex gap-2 text-sm justify-center items-center text-gray-600">
+                      <BedDouble className="h-4 w-4" />
+                      {item?.bedroom}
+                    </h2>
+                    <h2 className="flex gap-2 text-sm justify-center items-center text-gray-600 border-x-2 px-2">
+                      <Bath className="h-4 w-4" />
+                      {item?.bathroom}
+                    </h2>
+                    <h2 className="flex gap-2 text-sm justify-center items-center text-gray-600">
+                      <Ruler className="h-4 w-4" />
+                      {item?.area}
+                    </h2>
+                  </div>
+                </div>
               </div>
-            </div>
-          </div>
-        ))}
+            ))
+          : [1, 2, 3, 4, 5, 6, 7, 8].map((item, index) => (
+              <div
+                key={index}
+                className="h-[230px] w-full bg-slate-200 animate-pulse rounded-lg"
+              ></div>
+            ))}
       </div>
     </div>
   );
